3c6d8ac2a509bd5e728e3f2b4ffa2fdb9df72e1a
lmorningstar <jaster_ru@outlook.com>
2023-10-30 07:30:55 +0300

    Test

---
+++ __mocks__/server.js
@@ -0,0 +1,79 @@
+import { setupServer } from 'msw/node';
+import { rest } from 'msw';
+
+import _ from 'lodash';
+
+const getId = () => Number(_.uniqueId());
+
+const handlers = [
+  rest.post('/api/v1/lists/:listId/tasks', (req, res, ctx) => {
+    const { listId } = req.params;
+
+    const taskId = getId();
+    const task = {
+      id: taskId,
+      listId: parseInt(listId, 10),
+      text: req.body.text,
+      completed: false,
+      touched: Date.now(),
+    };
+    sessionStorage.setItem(taskId.toString(), JSON.stringify(task));
+
+    return res(
+      ctx.status(201),
+      ctx.json(task),
+    );
+  }),
+
+  rest.patch('/api/v1/tasks/:taskId', (req, res, ctx) => {
+    const { taskId } = req.params;
+
+    const task = JSON.parse(sessionStorage.getItem(taskId));
+    const updatedTask = {
+      ...task,
+      completed: req.body.completed,
+      touched: Date.now(),
+    };
+
+    sessionStorage.setItem(taskId, JSON.stringify(updatedTask));
+    return res(
+      ctx.status(201),
+      ctx.json(updatedTask),
+    );
+  }),
+
+  rest.delete('/api/v1/tasks/:taskId', (req, res, ctx) => {
+    const { taskId } = req.params;
+
+    sessionStorage.removeItem(taskId);
+    return res(
+      ctx.status(204),
+    );
+  }),
+
+  rest.post('/api/v1/lists', (req, res, ctx) => {
+    const listId = getId();
+    const list = {
+      id: listId,
+      name: req.body.name,
+      removable: true,
+    };
+    sessionStorage.setItem(listId.toString(), JSON.stringify(list));
+
+    return res(
+      ctx.status(201),
+      ctx.json(list),
+    );
+  }),
+
+  rest.delete('/api/v1/lists/:listId', (req, res, ctx) => {
+    const { listId } = req.params;
+
+    sessionStorage.removeItem(listId);
+    return res(
+      ctx.status(204),
+    );
+  }),
+];
+
+export const server = setupServer(...handlers);
---
+++ __tests__/application.test.jsx
@@ -0,0 +1,149 @@
+import '@testing-library/jest-dom';
+
+import React from "react";
+import {
+  render, screen, waitFor, within,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import TodoApp from '@hexlet/react-todo-app-with-backend';
+import { server } from '../__mocks__/server';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+beforeEach(() => {
+  render(<TodoApp />);
+
+  // screen.debug();
+});
+
+const addList = async (listName) => {
+  const newListInput = screen.getByRole('textbox', { name: /new list/i });
+
+  userEvent.type(newListInput, listName);
+  userEvent.click(screen.getByRole('button', { name: /add list/i }));
+
+  expect(newListInput).toHaveAttribute('readonly');
+  expect(await screen.findByRole('button', { name: listName })).toBeInTheDocument();
+  expect(newListInput).not.toHaveAttribute('readonly');
+};
+
+const addTask = async (taskText) => {
+  const newTaskInput = screen.getByLabelText('New task');
+
+  userEvent.type(newTaskInput, taskText);
+  userEvent.click(screen.getByRole('button', { name: 'Add' }));
+
+  expect(newTaskInput).toHaveAttribute('readonly');
+  expect(await screen.findByRole('checkbox', { name: taskText })).toBeInTheDocument();
+  expect(newTaskInput).not.toHaveAttribute('readonly');
+};
+
+describe('tasks', () => {
+  it('can be added to the different lists', async () => {
+    const taskName = 'task uno';
+    const listName = 'dos list';
+
+    await addList('primary list');
+    await addTask(taskName);
+    await addList(listName);
+    await userEvent.click(screen.getByRole('button', {name: listName}));
+
+    await waitFor(() => {
+      expect(screen.queryByText(taskName)).not.toBeInTheDocument();
+    });
+  });
+
+  it('can not be duplicated', async () => {
+    const taskName = 'repeat task';
+    await addList('primary list');
+    await addTask(taskName);
+    await addTask(taskName);
+
+    expect(screen.queryByText(`${taskName} already exists`)).toBeInTheDocument();
+    const tasksContainer = screen.getByTestId('tasks');
+    const taskItems = [...within(tasksContainer).getAllByRole('listitem')];
+    expect(taskItems).toHaveLength(1);
+  });
+
+  it('can not be empty', async () => {
+    userEvent.click(screen.getByRole('button', { name: 'Add' }));
+
+    expect(await screen.findByText('Required!')).toBeInTheDocument();
+  });
+
+  it('can be created, finished and removed', async () => {
+    const taskNames = ['first', 'second, third'];
+    await addList('primary list');
+
+    // Add tasks
+    for (const task of taskNames) {
+      await addTask(task);
+    }
+
+    // Change task state
+    const taskToClick = taskNames[1];
+    const checkBox = screen.getByRole('checkbox', { name: taskToClick });
+    userEvent.click(checkBox);
+    await waitFor(() => {
+      expect(checkBox).toBeChecked();
+    });
+
+    // Remove task
+    const taskItemToRemove = within(screen.getByTestId('tasks')).getByText(taskToClick).closest('li');
+    userEvent.click(within(taskItemToRemove).getByRole('button'));
+    await waitFor(() => {
+      expect(taskItemToRemove).not.toBeInTheDocument();
+    });
+  });
+});
+
+describe('lists', () => {
+  it('can not be duplicated', async () => {
+    const listName = 'primary list';
+    await addList(listName);
+    await addList(listName);
+
+    expect(screen.queryByText(`${listName} already exists`)).toBeInTheDocument();
+
+    const listsContainer = screen.getByTestId('lists');
+    const listItems = [...within(listsContainer).getAllByRole('listitem')];
+    expect(listItems).toHaveLength(1);
+  });
+
+  it('can not be empty', async () => {
+    userEvent.click(screen.getByRole('button', { name: /add list/i }));
+
+    expect(await screen.findByText('Required!')).toBeInTheDocument();
+  });
+
+  it('can be deleted', async () => {
+    const taskOne = 'task one';
+    const taskTwo = 'task two';
+    const listName = 'secondary list';
+
+    // Add one task for each list
+    await addList('primary list');
+    await addTask(taskOne);
+    await addList(listName);
+    userEvent.click(screen.getByRole('button', { name: listName }));
+    await waitFor(() => {
+      expect(screen.queryByText(taskOne)).not.toBeInTheDocument();
+    });
+
+    await addTask(taskTwo);
+
+    // Remove list
+    const listItem = within(screen.getByTestId('lists')).getByText(listName).closest('div');
+    userEvent.click(within(listItem).getByRole('button', { name: /remove list/i }));
+    await waitFor(() => {
+      expect(screen.queryByText(listName)).not.toBeInTheDocument();
+      expect(screen.queryByText(taskTwo)).not.toBeInTheDocument();
+    });
+
+    // Create the list again and check tasks
+    await addList(listName);
+    expect((screen.queryByText('Tasks list is empty'))).toBeInTheDocument();
+  });
+});
